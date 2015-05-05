b0ecdc7bb37a608c26b1a52584d9c1fd66f4765a
Ned Williamson <nedwilliamson@gmail.com>
2015-05-05 19:22:03 -0400

    remove dead test

--- test_tasks.py
+++ test_tasks.py
@@ -35,32 +35,5 @@ def test_calcagenda_assignments(l):
     cal.tasks = tasks
     cal.createAgenda()
 
-@given(int, int, bool, (year, month, day))
-def test_get_hours_per_day_list(hours, hours_done, work_today, due):
-    hours = abs(hours)
-    tl = TaskList()
-    today = datetime.date.today()
-    try:
-        due_dt = datetime.date(*due)
-    except ValueError:
-        #skip invalid dates
-        return
-    start_offset = 0 if work_today else 1
-    assume(due_dt > today + datetime.timedelta(start_offset)) #only care about tasks due in the future
-    hours_done %= (hours + 1) #make sure hours_done <= hours
-    task = Task("test", hours, hours_done, due_dt)
-    hours_day_list = tl._get_hours_per_day_list(task, work_today)
-    assert sum(hours_day_list) == (task.hours - task.hours_done)
-    assert len(hours_day_list) == (due_dt - today).days
-    if work_today:
-        assert 0 < len(set(hours_day_list)) < 3 #one or two hour options
-    else:
-        assert len(hours_day_list) == 1 or 0 < len(set(hours_day_list[1:])) < 3
-        assert hours_day_list[0] == 0
-    for i in xrange(len(hours_day_list)):
-        for j in xrange(i+1, len(hours_day_list)):
-            hours_day_list[i] >= hours_day_list[j]
-
 test_calcagenda_fixed()
 test_calcagenda_assignments()
-test_get_hours_per_day_list()
