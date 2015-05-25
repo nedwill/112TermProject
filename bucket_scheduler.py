9a20b6147d8cec0c89109cfcc33fc20565f21224
Ned Williamson <edwillia@andrew.cmu.edu>
2015-05-25 13:59:34 -0400

    start concept of grow_buckets

--- bucket_scheduler.py
+++ bucket_scheduler.py
@@ -4,6 +4,22 @@ tasks are (name, hours_remaining, last_bucket_num)
 tasks can only be placed into buckets < last_bucket_num
 """
 
+#for maximizing a given day
+def grow_buckets(buckets):
+	print buckets
+	for i, (hours_available, d) in enumerate(buckets):
+		while hours_available > 0:
+			unfinished_tasks = set()
+			for _hours, future_d in buckets[i+1:]:
+				for name in future_d:
+					unfinished_tasks.add(name)
+			if len(unfinished_tasks) == 0:
+				break #should return?
+	#given all unfinished tasks, add hours in order of earliest due if we won't break the max
+	#given unfinished tasks and non-max tasks, take intersection and inc the earliest
+	#if intersection empty, inc earliest task
+	print unfinished_tasks
+
 def bucket_scheduler(buckets, tasks):
 	tasks_sorted = sorted(tasks, key=lambda x: x[2])
 	for name, hours_remaining, last_bucket_num in tasks_sorted:
--- test_buckets.py
+++ test_buckets.py
@@ -1,12 +1,18 @@
 from hypothesis import given
 from hypothesis.strategies import lists, integers, tuples
-from bucket_scheduler import bucket_scheduler
+from bucket_scheduler import bucket_scheduler, grow_buckets
 
 #should schedule everything on first day
-def test_bucket_scheduler_not_enough_time():
+def test_bucket_scheduler_constrained():
 	buckets = [(8, {}), (0, {}), (8, {})]
 	tasks = [("test1", 8, 2)]
 	bucket_scheduler(buckets, tasks)
+
+def test_bucket_scheduler_dev():
+	buckets = [(8, {}), (0, {}), (8, {})]
+	tasks = [("test1", 4, 2), ("test2", 4, 3)]
+	buckets = bucket_scheduler(buckets, tasks)
+	grow_buckets(buckets)
 
 @given(lists(integers()), lists(tuples(integers(), integers())))
 def test_bucket_scheduler(buckets, tasks):
@@ -17,4 +23,5 @@ def test_bucket_scheduler(buckets, tasks):
 	print fixed_buckets, fixed_tasks
 	bucket_scheduler(fixed_buckets, fixed_tasks)
 
-test_bucket_scheduler()
+test_bucket_scheduler_dev()
+#test_bucket_scheduler()
