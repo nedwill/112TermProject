e770619c8284c0c8bee177776f6846821e1bded9
Ned Williamson <edwillia@andrew.cmu.edu>
2015-05-27 21:32:04 -0400

    steps set up in plannermachine

--- test_planner.py
+++ test_planner.py
@@ -11,12 +11,20 @@ class PlannerMachine(GenericStateMachine):
 	def steps(self):
 		add_task_strategy = strategy((just("add_task"), strings(), integers(), integers(), datetimes()))
 		delete_task_strategy = strategy((just("delete_task"), sampled_from(self.task_names)))
+		#TODO: support recurring tasks
+		add_fixed_task_strategy = strategy((just("add_fixed_task"), strings, datetimes(), datetimes()))
+		add_hours_strategy = strategy((just("add_hours"), sampled_from(self.task_names, integers())))
+		toggle_work_day_strategy = strategy((just("toggle_work_day"), sampled_from(range(7))))
+		reschedule_task_strategy = strategy((just("reschedule_task"), sampled_from(self.task_names), datetimes()))
+		set_max_hours_strategy = strategy((just("set_max_hours"), integers()))
+		toggle_max_days_strategy = strategy((just("toggle_max_days"), just(None))) #just a toggle, filler here
+		always_available = add_task_strategy | add_fixed_task_strategy | set_max_hours_strategy | toggle_max_days_strategy
 		if len(self.task_names) == 0:
-			return add_task_strategy
+			return always_available
 		else:
-			return add_task_strategy | delete_task_strategy
+			return always_available | delete_task_strategy | add_hours_strategy | toggle_work_day_strategy | reschedule_task_strategy
 
 	def execute_step(self, step):
 		action, data = step
-
-
+		#try to make schedule after doing action?
+		#catch it trying to make the new schedule and don't include the new task if it fails
